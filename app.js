3c694ac687f76163f4de7ed15999cab52edb7111
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-06-28 18:50:45 +0300

    Updatet the app.js,index.html, and style.css

--- app.js
+++ app.js
@@ -359,3 +359,43 @@ showCaseBtns.forEach((showCaseBtn) => {
 btnClose.addEventListener('click', () => {
   popup.style.display = 'none';
 });
+
+/* Form validation */
+
+const form = document.querySelector('form');
+const email = document.getElementById('mail');
+const submitBtn = document.getElementById('contact-btn');
+const error = submitBtn.nextElementSibling;
+
+const emailRegExp = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+window.addEventListener('load', () => {
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  email.className = isValid ? 'valid' : 'invalid';
+});
+
+email.addEventListener('input', () => {
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (isValid) {
+    email.className = 'valid';
+    error.textContent = '';
+    error.className = 'error';
+  } else {
+    email.className = 'invalid';
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const isValid = email.value.length === 0 || emailRegExp.test(email.value);
+  if (!isValid) {
+    email.className = 'invalid';
+    error.textContent = 'The email text has to be in small letters';
+    error.className = 'error active';
+  } else {
+    email.className = 'valid';
+    error.textContent = '';
+    error.className = 'error';
+  }
+});
